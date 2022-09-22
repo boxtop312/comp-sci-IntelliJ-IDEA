d608d010aacf4b75d5fd61190139595d7ee17ca0
Alex Miranda <owsleymiranda@gmail.com>
2022-09-22 15:24:52 -0500

    commmit

---
+++ src/Magpie2.java
@@ -0,0 +1,84 @@
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ * 		    Handles responding to simple words and phrases
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie2
+{
+	/**
+	 * Get a default greeting
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, let's talk.";
+	}
+
+	/**
+	 * Gives a response to a user statement
+	 *
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+
+		if (statement.indexOf("no") >= 0 || statement.indexOf("No") >= 0) {
+			response = "Why so negative?";
+		} else if (statement.indexOf("Sun") >= 0 || statement.indexOf("sun") >= 0) {
+			response = "The sun is very hot.";
+	    } else if (statement.trim().length() == 0) {
+	    	response = "Say something, please.";
+	    } else {
+			response = getRandomResponse();
+		}
+		return response;
+	}
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 6;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			 response = "The HORSE is a noble animal.";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "You should go give Elliot Gorokhovsky five dollars.";
+		}
+		return response;
+	}
+}
---
+++ src/MagpieMain.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+/**
+ * A simple class to run the Magpie class.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class MagpieMain{
+
+	/**
+	MAGPIE RUNNER 2
+	 Create a Magpie, give it user input, and print its replies.
+	 */
+	public static void main(String[] args){
+		Magpie2 maggie = new Magpie2();
+		
+		System.out.println (maggie.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		
+		while (!statement.equals("Bye"))
+		{
+			System.out.println (maggie.getResponse(statement));
+			statement = in.nextLine();
+		}
+	}
+
+	// /**
+	// 	MAGPIE RUNNER 3
+	//  Create a Magpie, give it user input, and print its replies.
+	//  */
+	// public static void main(String[] args)
+	// {
+	// 	Magpie3 maggie = new Magpie3();
+		
+	// 	System.out.println (maggie.getGreeting());
+	// 	Scanner in = new Scanner (System.in);
+	// 	String statement = in.nextLine();
+		
+	// 	while (!statement.equals("Bye"))
+	// 	{
+	// 		System.out.println (maggie.getResponse(statement));
+	// 		statement = in.nextLine();
+	// 	}
+	// }
+
+	// /**
+	//  MAGPIE RUNNER 4
+	//  * Create a Magpie, give it user input, and print its replies.
+	//  */
+	// public static void main(String[] args)
+	// {
+	// 	Magpie4 maggie = new Magpie4();
+		
+	// 	System.out.println (maggie.getGreeting());
+	// 	Scanner in = new Scanner (System.in);
+	// 	String statement = in.nextLine();
+		
+	// 	while (!statement.equals("Bye"))
+	// 	{
+	// 		System.out.println (maggie.getResponse(statement));
+	// 		statement = in.nextLine();
+	// 	}
+	// }
+
+
+	// /**
+	// 	MAGPIE RUNNER 5
+	//  * Create a Magpie, give it user input, and print its replies.
+	//  */
+	// public static void main(String[] args)
+	// {
+	// 	Magpie5 maggie = new Magpie5();
+		
+	// 	System.out.println (maggie.getGreeting());
+	// 	Scanner in = new Scanner (System.in);
+	// 	String statement = in.nextLine();
+		
+	// 	while (!statement.equals("Bye"))
+	// 	{
+	// 		System.out.println (maggie.getResponse(statement));
+	// 		statement = in.nextLine();
+	// 	}
+	// }
+
+}
