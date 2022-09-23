6a6eae76b4c06e8d6bc9933c88d7189505321440
Alex Miranda <owsleymiranda@gmail.com>
2022-09-23 15:29:21 -0500

    commmit

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -32,13 +32,24 @@ public class Magpie2
 	{
 		String response = "";
 
-		if (statement.indexOf("no") >= 0 || statement.indexOf("No") >= 0) {
+		if (statement.indexOf("no ") >= 0 || statement.indexOf("No ") >= 0) {
 			response = "Why so negative?";
 		} else if (statement.indexOf("Sun") >= 0 || statement.indexOf("sun") >= 0) {
 			response = "The sun is very hot.";
 	    } else if (statement.trim().length() == 0) {
 	    	response = "Say something, please.";
-	    } else {
+	    } else if(statement.indexOf("Cat")>= 0 || statement.indexOf("cat")>=0||statement.indexOf("pet")>=0||statement.indexOf("Pet")>=0||statement.indexOf("Dog")>=0||statement.indexOf("dog")>=0){
+			response = "I have a cat, its name it Jumbo";
+		} else if(statement.indexOf("yin ")>=0||statement.indexOf("Yin ")>=0){
+			response = "You should be nicer to Mr. Yin";
+		} else if(statement.indexOf("iphone")>=0||statement.indexOf("iPhone")>=0||statement.indexOf("IPhone")>=0||statement.indexOf("Iphone")>=0){
+			response = "iPhones are worse than androids";
+		} else if(statement.indexOf("bot")>=0||statement.indexOf("Bot")>=0){
+			response = "I'm not a chatbot, how dare you substantiate that I might be!";
+		} else if(statement.indexOf("pda")>=0||statement.indexOf("PDA")>=0||statement.indexOf("Palm")>=0||statement.indexOf("palm")>=0){
+			response="I like PDA's.";
+		}
+		else {
 			response = getRandomResponse();
 		}
 		return response;
@@ -50,34 +61,27 @@ public class Magpie2
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 6;
+		final int NUMBER_OF_RESPONSES = 8;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
 
-		if (whichResponse == 0)
-		{
+		if (whichResponse == 0) {
 			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
+		} else if (whichResponse == 1) {
 			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
+		} else if (whichResponse == 2) {
 			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
+		} else if (whichResponse == 3) {
 			response = "You don't say.";
-		}
-		else if (whichResponse == 4)
-		{
-			 response = "The HORSE is a noble animal.";
-		}
-		else if (whichResponse == 5)
-		{
-			response = "You should go give Elliot Gorokhovsky five dollars.";
+		} else if (whichResponse == 4) {
+			 response = "The squirel can surive falling from terminal velocity.";
+		} else if (whichResponse == 5) {
+			response = "You should go give Alex Miranda five dollars.";
+		} else if (whichResponse == 6){
+			response = "There are 48 regular Polyhedra";
+		} else if (whichResponse == 7){
+			response = "You should listen to Legaly Blonde: The Musical";
 		}
 		return response;
 	}
--- src/MagpieMain.java
+++ src/MagpieMain.java
@@ -11,38 +11,38 @@ public class MagpieMain {
 	MAGPIE RUNNER 2
 	 Create a Magpie, give it user input, and print its replies.
 	 */
-	public static void main(String[] args){
-		Magpie2 maggie = new Magpie2();
-		
-		System.out.println (maggie.getGreeting());
-		Scanner in = new Scanner (System.in);
-		String statement = in.nextLine();
-		
-		while (!statement.equals("Bye"))
-		{
-			System.out.println (maggie.getResponse(statement));
-			statement = in.nextLine();
-		}
-	}
+//	public static void main(String[] args){
+//		Magpie2 maggie = new Magpie2();
+//
+//		System.out.println (maggie.getGreeting());
+//		Scanner in = new Scanner (System.in);
+//		String statement = in.nextLine();
+//
+//		while (!statement.equals("Bye"))
+//		{
+//			System.out.println (maggie.getResponse(statement));
+//			statement = in.nextLine();
+//		}
+//	}
 
-	// /**
-	// 	MAGPIE RUNNER 3
-	//  Create a Magpie, give it user input, and print its replies.
-	//  */
-	// public static void main(String[] args)
-	// {
-	// 	Magpie3 maggie = new Magpie3();
+	 /**
+	 	MAGPIE RUNNER 3
+	  Create a Magpie, give it user input, and print its replies.
+	  */
+	 public static void main(String[] args)
+	 {
+	 	Magpie3 maggie = new Magpie3();
 		
-	// 	System.out.println (maggie.getGreeting());
-	// 	Scanner in = new Scanner (System.in);
-	// 	String statement = in.nextLine();
+	 	System.out.println (maggie.getGreeting());
+	 	Scanner in = new Scanner (System.in);
+	 	String statement = in.nextLine();
 		
-	// 	while (!statement.equals("Bye"))
-	// 	{
-	// 		System.out.println (maggie.getResponse(statement));
-	// 		statement = in.nextLine();
-	// 	}
-	// }
+	 	while (!statement.equals("Bye"))
+	 	{
+	 		System.out.println (maggie.getResponse(statement));
+	 		statement = in.nextLine();
+	 	}
+	 }
 
 	// /**
 	//  MAGPIE RUNNER 4
