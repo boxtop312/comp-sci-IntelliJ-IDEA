6c11bff45a72b34991473deb1bf05c79110d554c
Alex Miranda <owsleymiranda@gmail.com>
2022-09-27 15:26:12 -0500

    commmit

--- src/Magpie3.java
+++ src/Magpie3.java
@@ -68,43 +68,64 @@ public class Magpie3
 	 * @return the index of the first occurrence of goal in
 	 *         statement or -1 if it's not found
 	 */
-	private int findKeyword(String statement, String goal, int startPos){
+//	private int findKeyword(String statement, String goal, int startPos){
+//		String phrase = statement.trim().toLowerCase();
+//		goal = goal.toLowerCase();
+//
+//		// The only change to incorporate the startPos is in
+//		// the line below
+//		int psn = phrase.indexOf(goal, startPos);
+//
+//		// Refinement--make sure the goal isn't part of a
+//		// word
+//		while (psn >= 0){
+//			// Find the string of length 1 before and after
+//			// the word
+//			String before = " ", after = " ";
+//			if (psn > 0){
+//				before = phrase.substring(psn - 1, psn);
+//			}
+//			if (psn + goal.length() < phrase.length()){
+//				after = phrase.substring(psn + goal.length(),psn + goal.length() + 1);
+//			}
+//
+//			// If before and after aren't letters, we've found the word
+//			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before is not a
+//									// letter
+//					&& ((after.compareTo("a") < 0) || (after
+//							.compareTo("z") > 0))){
+//				return psn;
+//			}
+//
+//			// The last position didn't work, so let's find
+//			// the next, if there is one.
+//			psn = phrase.indexOf(goal, psn + 1);
+//
+//		}
+//
+//		return -1;
+//	}
+	private int findKeyword(String statement, String goal, int startPos) {
 		String phrase = statement.trim().toLowerCase();
 		goal = goal.toLowerCase();
-
-		// The only change to incorporate the startPos is in
-		// the line below
 		int psn = phrase.indexOf(goal, startPos);
-
-		// Refinement--make sure the goal isn't part of a
-		// word
-		while (psn >= 0){
-			// Find the string of length 1 before and after
-			// the word
+		while (psn >= 0) {
 			String before = " ", after = " ";
-			if (psn > 0){
+			if (psn > 0) {
 				before = phrase.substring(psn - 1, psn);
 			}
-			if (psn + goal.length() < phrase.length()){
-				after = phrase.substring(psn + goal.length(),psn + goal.length() + 1);
+			if (psn + goal.length() < phrase.length()) {
+				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1);
 			}
-
-			// If before and after aren't letters, we've found the word
-			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before is not a
-									// letter
-					&& ((after.compareTo("a") < 0) || (after
-							.compareTo("z") > 0))){
+			/* determine the values of psn, before, and after at this point in the method. */
+			if (((before.compareTo ("a") < 0 ) || (before.compareTo("z") > 0)) && ((after.compareTo ("a") < 0 ) || (after.compareTo("z") > 0))) {
 				return psn;
 			}
-
-			// The last position didn't work, so let's find
-			// the next, if there is one.
 			psn = phrase.indexOf(goal, psn + 1);
-
 		}
-
 		return -1;
 	}
+
 
 	/**
 	 * Search for one word in phrase. The search is not case
--- src/StringExplorer.java
+++ src/StringExplorer.java
@@ -10,7 +10,10 @@ public class StringExplorer
 	public static void main(String[] args)
 	{
 		String sample = "The quick brown fox jumped over the lazy dog.";
-		
+		int notFoundPsn = sample.indexOf("slow");
+		System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn);
+
+
 		//  Demonstrate the indexOf method.
 		int position = sample.indexOf("quick");
 		System.out.println ("sample.indexOf(\"quick\") = " + position);
